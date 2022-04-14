181dd595a1b46cc36ec1378202bdd7ac966f2163
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-04-14 10:38:21 +0530

    removed build error

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = (props) => {
     return (
         <div className="p-5 pb-0 border-b dark:border-gray-800">
             <div className="flex flex-wrap justify-between items-center">
-                <div className="text-2xl bg-green-600 text-gray-50 px-2 py-1 rounded dark:bg-green-900"> <Link to="/"><h1>Google 🔍</h1></Link></div>
+                <div className="text-2xl bg-green-600 text-gray-50 px-2 py-1 rounded dark:bg-green-900"> <Link to="/"><h1>Search-X 🔍</h1></Link></div>
                 <div className="bg-gray-800 text-gray-500 p-2 rounded-full hover:shadow-xl dark:bg-gray-200 cursor-pointer"><button onClick={themeHandler}>{props.darkTheme ? "Light💡" : "Dark🌙"}</button></div>
             </div>
             <Search />
